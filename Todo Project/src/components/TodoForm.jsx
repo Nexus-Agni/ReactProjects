02c1379c8f2953c0f3265750e62f41baf3c4390d
Nexus-Agni <agnibhachakraborty11@gmail.com>
2024-02-08 22:14:23 +0530

    updated

--- Todo Project/src/components/TodoForm.jsx
+++ Todo Project/src/components/TodoForm.jsx
@@ -11,7 +11,7 @@ function TodoForm() {
             return 
         } else {
             addTodo({todo, completed : false})
-            setTodo("")
+            setTodo(" ")
         }
     }
 
@@ -22,6 +22,7 @@ function TodoForm() {
                 placeholder="Write Todo..."
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
                 onChange={(e)=>setTodo(e.target.value)}
+                value={todo}
             />
             <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
                 Add
